cad5456d5bd1c37e2386bb2f825435f1fa3aa069
kyi <kyipersonal@gmail.com>
2022-07-21 20:58:47 -0500

    progress

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import * as React from 'react'
-import * as css from '../components/layout.module.css'
-import Layout from '../components/layout'
+import * as css from '/src/components/layout.module.css'
+import Layout from '/src/components/layout'
 
-import cumulative_heatmap from '../images/cumulative_heatmap.png'
-import exampleubertripgif from '../images/tripgif.gif'
-import example_marker_map from '../images/example marker map.png'
-import example_fmm_map from '../images/example fmm map.png'
-import example_heatmap from '../images/example heatmap.png'
+import cumulative_heatmap from '/src/images/cumulative_heatmap.png'
+import exampleubertripgif from '/src/images/tripgif.gif'
+import exampleheatmapgif from '/src/images/heatmapgif.gif'
+import example_marker_map from '/src/images/example marker map.png'
+import example_fmm_map from '/src/images/example fmm map.png'
+import example_heatmap from '/src/images/example heatmap.png'
 
 
 
@@ -67,22 +68,24 @@ const IndexPage = () => {
       <h2>Creating Our Maps</h2>
       <p>
         There are some formatting issues with different EPSG coordinate systems, but after transforming our data, we can now map it into Folium.
-        <br /><br />
 
         <img src={example_marker_map} className={css.maps} />
-        <br /><br />
 
         Here's an example of a Folium map populated with our coordinates. Each marker is a coordinate that we located from Georeferencer. As you can see from the red colored path, these points aren't enough to get us an accurate representation of the Uber trip path.
         This is where FMM comes in.
-        <br /><br />
+
         <img src={example_fmm_map} className={css.maps} />
+
         FMM gives us a much more accurate set of coordinates that is matched with real world roads. Represented in green, the FMM path is a drastic improvement compared to our manual coordinate path.
         <br /><br />
         With an accurate represenation of our path, we can finally convert this FMM path to a heatmap.
-        <br /><br />
+
         <img src={example_heatmap} className={css.maps} />
 
-        Now, with th
+        Now, we know how to convert our data into an FMM path and heatmap. We can do this for every Uber trip we have.
+
+        <img src={exampleheatmapgif} className={css.maps} />
+
       </p>
 
 
